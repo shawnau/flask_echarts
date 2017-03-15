b2cb5988270eccd3993b6ca5b06da1f100e9a2e9
xiaoxuan <shawn_au@outlook.com>
2017-03-16 00:16:25 +0800

    Update view.py
    change host

--- visualization/view.py
+++ visualization/view.py
@@ -73,4 +73,4 @@ def home_page():
     return render_template('index.html', data=data)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+    app.run(host='0.0.0.0', port=8080)
